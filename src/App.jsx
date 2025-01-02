d92a8fb969eeaa4765274d11c49774fc912e254e
RETO-INDIA <priyanshutest686@gmail.com>
2025-01-02 18:10:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CheckoutPage from "./components/CheckOutPage/CheckoutPage";
+
+function App() {
+  return (
+    <>
+      <div className="navbar">NavBar</div>
+      <CheckoutPage />
+    </>
+  );
+}
+
+export default App;
